3345cfe5ef18a6678b61e52d6b45b21c689ada64
Evan Moran <moraneva@cse.msu.edu>
2017-01-23 21:04:54 -0500

    Have two different benchmarks for functions. Need to combine them and use the compare function

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -12,11 +12,33 @@ var routes = {
     }
 };
 
-apiBenchmark.measure(services, routes, { minSamples: 100 }, (err, results) => {
+apiBenchmark.compare(services, routes, { minSamples: 100 }, (err, results) => {
 
     apiBenchmark.getHtml(results, (error, html) => {
 
-        fs.writeFile("results.html", html, (err) => {
+        fs.writeFile("results1.html", html, (err) => {
+            console.log('Written to file');
+        });
+        // now save it yourself to a file and enjoy 
+    });
+});
+
+services = {
+	server1: "https://v03codwpc3.execute-api.us-east-2.amazonaws.com/"
+};
+
+routes = {
+	route1:{
+		method: 'get',
+		route: 'Prod'
+	}
+}
+
+apiBenchmark.compare(services, routes, { minSamples: 100 }, (err, results) => {
+
+    apiBenchmark.getHtml(results, (error, html) => {
+
+        fs.writeFile("results2.html", html, (err) => {
             console.log('Written to file');
         });
         // now save it yourself to a file and enjoy 
